d78317d24d9508d905d5ebb179fa9b7a5e1ba6cc
Maple <sean.fleming@gmail.com>
2025-02-02 04:20:56 -0500

    Added Mapbox

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import Map from './components/Map'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <NavBar />
+        <Map />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
